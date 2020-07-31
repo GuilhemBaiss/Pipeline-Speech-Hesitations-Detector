4bee2ff3f7038f2ae3d3b3a2ce6a2fcaa2e743e9
GuilhemBaiss <guiguibais@gmail.com>
2020-07-31 10:29:00 -0400

    global structure

---
+++ setup.py
@@ -0,0 +1,22 @@
+# import setuptools
+
+# with open("README.md", "r") as fh:
+#     long_description = fh.read()
+
+# setuptools.setup(
+#     name="pipeline-speech-hesitations-detector",
+#     version="0.0.1",
+#     author="Guilhem Baissus",
+#     author_email="guilhem.baissus@insa-lyon.fr",
+#     description="A tool to detect hesitations from sound files",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     url="",
+#     packages=setuptools.find_packages(),
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     python_requires='>=3.6',
+# )
